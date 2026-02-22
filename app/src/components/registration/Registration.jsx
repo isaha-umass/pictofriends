4ed53d369590ec98f118407b5369c258ee0b8f63
Avanthika Kethu <avanthikakethu@gmail.com>
2026-02-21 22:32:18 -0500

    did the css for the registration

--- app/src/components/registration/Registration.jsx
+++ app/src/components/registration/Registration.jsx
@@ -2,6 +2,7 @@ import {signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, createU
 import { useState } from 'react'
 import {app} from "../../../firebase"
 import { Link } from 'react-router-dom'
+import "./Registration.css"
 
 function Registration() {
     const [email, setEmail] = useState('');
@@ -38,28 +39,42 @@ function Registration() {
         }
     }
     return (
-        <div>
-            <form>
-                <input type = "text" 
+        <div className="registration-container">
+            <img
+            src="/logo.jpg"
+            alt="Logo"
+            className="top-image"
+            />
+
+            <div className="registration-box">
+            <form className="registration-form" onSubmit={(e)=> e.preventDefault()}>
+                <input 
+                type = "text" 
                 value = {email}
                 onChange = {(e)=>setEmail(e.target.value)}
-                placeholder = "Enter your email..."
+                placeholder = "email"
                 />
 
-                <input type = "password" 
+                <input 
+                type = "password" 
                 value = {password}
                 onChange = {(e)=>setPassword(e.target.value)}
-                placeholder = "Enter your password..."
+                placeholder = "password"
                 />
                 <Link to="/canvas">
                     <button type = "submit" onClick = {handleLogin}> Log in </button>
                 </Link>
-            </form>
+            </form> <br/>
+            <div className="extra-buttons">
 
-            <button onClick={handleSignup}> Sign Up </button>
+            <button onClick={handleSignup} className="signup-btn"> Sign Up </button>
 
-            <button onClick = {handleGoogleLogin}> Continue with Google </button>
+            <button onClick = {handleGoogleLogin} className="google-btn">Continue with Google</button>
+            </div>
+            </div>
         </div>
+
+    
     )
 
 }
